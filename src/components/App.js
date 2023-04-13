34fc556599e850576c453c42dadebd82ab78c080
Samuel Lakra <samuellakra1290@gmail.com>
2023-04-13 18:23:28 +0530

    1st commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/App.css';
 const App = () => {
+
+  const [divName ,setDivName] = useState();
+  const [shapes ,setShapes] = useState([]);
+
+  function addShape(){
+    setShapes(<div className={divName}></div>);
+  }
+
+  function handleClick(e){
+    console.log(e.target.value);
+    setDivName(e.target.value);
+  }
 
   return (
     <div id="main">
       <div id="shape-creator">
-        
-
+        <select defaultValue="square" onClick={handleClick}>
+          <option value="square">Square</option>
+          <option value="circle">Circle</option>
+        </select>
+        <button onClick={addShape}>Add shape</button>
       </div>
       <div id="shapes-holder">
+        {shapes}
       </div>
     </div>
   )
